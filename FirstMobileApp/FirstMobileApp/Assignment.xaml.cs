f184a83998ad42c1cfff53526fcaeb3272420eb1
JAYaseelan <jayaseelanpm@hotmail.com>
2024-02-13 16:48:30 +0530

    Add project files.

---
+++ FirstMobileApp/FirstMobileApp/Assignment.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace FirstMobileApp
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class Assignment : ContentPage
+	{
+		public Assignment ()
+		{
+			InitializeComponent ();
+		}
+
+        private void PostData(object sender, EventArgs e)
+        {
+            bool IsValid = true;
+            if(string.IsNullOrWhiteSpace(fName.Text))
+            {
+                IsValid=false;
+                DisplayAlert("First Name", "First name must not be empty", "Ok");
+            }
+            else if (string.IsNullOrWhiteSpace(lName.Text))
+            {
+                IsValid = false;
+                DisplayAlert("Last Name", "Last name must not be empty", "Ok");
+            }
+            else if (string.IsNullOrWhiteSpace(comments.Text))
+            {
+                IsValid = false;
+                DisplayAlert("Comments", "Comments must not be empty", "Ok");
+            }
+            if(IsValid)
+            {
+                string result = $"Name: {fName.Text} {lName.Text} \n Comments: {comments.Text}";
+                DisplayAlert("Assignment", result, "Ok");
+            }
+        }
+
+        private void ClearData(object sender, EventArgs e)
+        {
+            fName.Text = string.Empty;
+            lName.Text = string.Empty;
+            comments.Text = string.Empty;
+        }
+    }
+}
